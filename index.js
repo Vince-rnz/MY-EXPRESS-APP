ef473bad8c26ad9571cb707ab21e5094a75c515c
vince <vincerenzocawayandelfin@gmail.com>
2025-06-09 22:23:35 +0800

    i add css

--- index.js
+++ index.js
@@ -2,66 +2,43 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const items = ['Apple','Banana','Orange'];
+const items = ['Apple', 'Banana', 'Orange'];
 
-//Server static files from the "public" folder
+// Server static files from the "public" folder
 app.use(express.static('public'));
 app.use(express.json()); // Middleware to parse JSON bodies
 
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
+    console.log(`${req.method} ${req.url}`);
+    next();
 });
 
 app.get('/items', (req, res) => {
-  res.json(items);
+    res.json(items);
 });
 
 app.post('/items', (req, res) => {
-  const newItem = req.body.item;
-  items.push(newItem);
-  res.json(items);
+    const newItem = req.body.item;
+    items.push(newItem);
+    res.json(items);
 });
 
 app.post('/submit', (req, res) => {
-  const data = req.body;
-  res.send(`Received: ${JSON.stringify(data)}`);
+    const data = req.body;
+    res.send(`Received: ${JSON.stringify(data)}`);
 });
-
-app.delete('/items/:id', (req, res) => {
-  const index = parseInt(req.params.id);
-  if (index >= 0 && index < items.length) {
-    items.splice(index, 1);
-    res.json(items);
-  } else {
-    res.status(404).json({ error: 'Item not found' });
-  }
-});
-
-app.put('/items/:id', (req, res) => {
-  const index = parseInt(req.params.id);
-  const newName = req.body.item;
-  if (index >= 0 && index < items.length) {
-    items[index] = newName;
-    res.json(items);
-  } else {
-    res.status(404).json({ error: 'Item not found' });
-  }
-});
-
-
 
 // Define a route for the home page
 app.get('/about', (req, res) => {
-  res.send('About Us');
+    res.send('About Us');
 });
 
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
